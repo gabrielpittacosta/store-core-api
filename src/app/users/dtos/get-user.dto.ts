060e5f13b315ea47b526d0c536d960c843da26a2
gabrielpittacosta <gabrielpittacosta@gmail.com>
2020-07-07 18:05:02 -0300

    feat(users): set getUserDto and configure it on controller

--- src/app/users/dtos/get-user.dto.ts
+++ src/app/users/dtos/get-user.dto.ts
@@ -1,20 +1,57 @@
-import { Expose } from 'class-transformer'
-import { User } from '../entities/user.entity'
-import { InsertResult } from 'typeorm'
+import { Exclude, Expose } from 'class-transformer'
+import { UserRole } from '../enums/user-role.enum'
 
 export class GetUserDto {
+  @Exclude()
+  id: number
+
+  @Exclude()
   name: string
 
-  @Expose({ name: 'last_name' })
+  @Exclude()
   lastName: string
 
   login: string
 
-  constructor(partial: any) {
-    Object.assign(this, {
-      name: partial.name,
-      lastName: partial.lastName,
-      login: partial.login,
-    })
+  @Exclude()
+  email: string
+
+  @Exclude()
+  password: string
+
+  @Exclude()
+  roleId: UserRole
+
+  @Exclude()
+  addressId: number
+
+  @Expose({ name: 'full_name' })
+  get fullName(): string {
+    return `${this.name} ${this.lastName}`
+  }
+
+  @Expose({ name: 'role' })
+  get type(): string {
+    let roleName: string
+    switch (this.roleId['roleId']) {
+      case (this.roleId['roleId'] = 1):
+        roleName = 'CASH_REGISTER'
+        break
+      case (this.roleId['roleId'] = 2):
+        roleName = 'MANAGER'
+        break
+      case (this.roleId['roleId'] = 3):
+        roleName = 'CLEANING'
+        break
+      case (this.roleId['roleId'] = 4):
+        roleName = 'STORAGE'
+        break
+    }
+
+    return roleName
+  }
+
+  constructor(partial: Partial<GetUserDto>) {
+    Object.assign(this, partial)
   }
 }
