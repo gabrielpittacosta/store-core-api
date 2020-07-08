a6d6b1c610aca192d353c05181f70ad552051750
gabrielpittacosta <gabrielpittacosta@gmail.com>
2020-07-08 15:54:54 -0300

    feat(auth): initial structure for user authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,11 @@ import { Module } from '@nestjs/common'
 import { UsersModule } from './users/users.module'
 import { DatabaseModule } from '../database/database.module'
 import { ConfigModule } from '@nestjs/config'
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, DatabaseModule],
+  imports: [ConfigModule.forRoot(), UsersModule, DatabaseModule, AuthModule],
+  providers: [AuthService],
 })
 export class AppModule {}
