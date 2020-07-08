fa10acbf191ebbc84ded7be1299c4687f8f62e61
gabrielpittacosta <gabrielpittacosta@gmail.com>
2020-07-08 17:39:10 -0300

    feat(auth): configure authenticathion methods

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,4 +1,22 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common'
+import { UsersModule } from '../users/users.module'
+import { AuthService } from './auth.service'
+import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt'
+import { JwtConstants } from './constants'
+import { JwtStrategy } from './jwt.strategy'
+import { LocalStrategy } from './local.strategy'
 
-@Module({})
+@Module({
+  imports: [
+    forwardRef(() => UsersModule),
+    PassportModule,
+    JwtModule.register({
+      secret: JwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [AuthService, JwtStrategy, LocalStrategy],
+  exports: [AuthService],
+})
 export class AuthModule {}
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { UsersService } from '../users/users.service'
+import { JwtService } from '@nestjs/jwt'
+import { GetUserDto } from '../users/dtos/get-user.dto'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(login: string, pass: string) {
+    const user = await this.usersService.findByLogin(login)
+    if (user && user.password === pass) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password, ...result } = user
+
+      return result
+    }
+
+    return null
+  }
+
+  async login(user: GetUserDto) {
+    const payload = {
+      login: user.login,
+      sub: user.id,
+      name: user.fullName,
+      role: user.type,
+    }
+
+    return {
+      name: payload.name,
+      login: payload.login,
+      role: payload.role,
+      // eslint-disable-next-line @typescript-eslint/camelcase
+      access_token: this.jwtService.sign(payload),
+    }
+  }
+}
