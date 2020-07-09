b4f7c1ef05d73b641a2f827892fc81b595d0456a
gabrielpittacosta <gabrielpittacosta@gmail.com>
2020-07-08 23:02:27 -0300

    feat(products): initial structure for products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,11 @@ import { UsersModule } from './users/users.module'
 import { DatabaseModule } from '../database/database.module'
 import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module'
+import { ProductService } from './product/product.service';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, DatabaseModule, AuthModule],
+  imports: [ConfigModule.forRoot(), UsersModule, DatabaseModule, AuthModule, ProductModule],
+  providers: [ProductService],
 })
 export class AppModule {}
